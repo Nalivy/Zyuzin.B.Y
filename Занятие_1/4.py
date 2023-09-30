bbefed79aa17549cfd1cb69f46f8ce3f017bc77c
Nalivy <dn7446656@gmail.com>
2023-10-01 01:24:22 +0300

    .

---
+++ Занятие_1/4.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+second=float(input("Введите колличество секунд: "))
+minuts=float(second / 60)
+hours=float(second / 3600)
+days=float(second / 86400)
+print(f"Минуты: {'%.2f'% minuts}\nЧасы: {'%.4f'% hours}\nДни: {'%.5f'% days}")
