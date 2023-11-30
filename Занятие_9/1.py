d84aca3850b1f85ec6d947b95c56de5868ae34fa
Nalivy <dn7446656@gmail.com>
2023-11-30 22:36:30 +0300

    .

--- Занятие_9/1.py
+++ Занятие_9/1.py
@@ -1,23 +1,21 @@
 # -- coding: utf-8 --
+#вариант 8
 
-# Вариант 6
+def divide_diagonal(matrix, k):
+    n = len(matrix)
+    if k <= 0 or k > n:
+        raise ValueError("Неверное значение K")
+    diagonal_element = matrix[k-1][k-1]
+    if diagonal_element == 0:
+        raise ValueError("Диагональный элемент равен нулю")
+    for j in range(n):
+        matrix[k-1][j] /= diagonal_element
 
-MATRIX = [
-        [1,6,3],
-        [4,8,2],
-        [7,9,4]
-        ]
-
-def max_in_stroke(matrix):
-    result = []
-    for i in matrix:
-        result.append(max(i))
-    return ', '.join(map(str,result))
-
-def min_in_column(matrix):
-    result = []
-    for i in zip(*matrix):
-        result.append(min(i))
-    return ', '.join(map(str,result))
-
-print(f'Максимальные значения в строках: {max_in_stroke(MATRIX)}\nМинимальные значения в столбцах: {min_in_column(MATRIX)}')
+    return matrix
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]]
+k = 2
+result = divide_diagonal(matrix, k)
+print(result)
